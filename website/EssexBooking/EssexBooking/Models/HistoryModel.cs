e2e4a6e49c83ec5b574e5c04bff954addefe6ea1
shoopi <shoopi@gmail.com>
2012-02-26 13:03:29 +0000

    New HistoryModel
    Signed-off-by: shoopi <shoopi@gmail.com>

---
+++ website/EssexBooking/EssexBooking/Models/HistoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssexBooking.Models
+{
+    public class HistoryModel
+    {
+        public string UserID { get; set; }
+        public string email { get; set; }
+        public Booking booking { get; set; }
+
+        public void history(Guid userId)
+        {
+            ASPNETDBEntities db = new ASPNETDBEntities();
+            var query = db.Bookings.SingleOrDefault(x => x.customer_id == userId);
+        }
+    }
+}
