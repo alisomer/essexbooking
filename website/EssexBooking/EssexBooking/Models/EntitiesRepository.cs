94655fb0b233415f039bdbad22a843cfe152c9a9
aiswaryasp <aiswaryasprem@gmail.com>
2012-02-26 16:18:01 +0000

    Telesales view created

---
+++ website/EssexBooking/EssexBooking/Models/EntitiesRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssexBooking.Models
+{
+    public class EntitiesRepository
+    {
+        private ASPNETDBEntities entities = new ASPNETDBEntities();
+
+        public IQueryable<Resort> GetResorts()
+        {
+            return entities.Resorts;
+        }
+        public IQueryable<HotelType> GetHotelTypes()
+        {
+            return entities.HotelTypes;
+        }
+        public IQueryable<TravelType> GetTravelTypes()
+        {
+            return entities.TravelTypes;
+        }
+        public IQueryable<Extra> GetExtras()
+        {
+            return entities.Extras;
+        }
+
+    }
+}
