5a3a358c3b46697f2ae59ebbf1a8d67c29fe4e53
ali <asomer@essex.ac.uk>
2012-03-18 00:17:09 +0000

    refactored the temp_id to booking_id fixed and refactored the payment view and booking controller

--- website/EssexBooking/EssexBooking/Scripts/AjaxProxy.js
+++ website/EssexBooking/EssexBooking/Scripts/AjaxProxy.js
@@ -17,9 +17,9 @@ function RemoveHotelFromCart(hotel_id, callback) {
 }
 */
 
-function RemoveBookingFromCart(temp_id, callback) {
+function RemoveBookingFromCart(booking_id, callback) {
     $.ajax({
-        data: { temp_id: temp_id },
+        data: { booking_id: booking_id },
         url: '/Ajax/RemoveBookingFromCart',
         cache: false,
         success: callback
@@ -37,36 +37,36 @@ function UpdateBooking(data, callback) {
 
 }
 
-function AddExtraToBooking(temp_id, extra_id, number, extra_date, callback) {
+function AddExtraToBooking(booking_id, extra_id, number, extra_date, callback) {
     $.ajax({
-        data: { 'temp_id': temp_id, 'extra_id': extra_id, 'number': number, 'extra_date': extra_date},
+        data: { 'booking_id': booking_id, 'extra_id': extra_id, 'number': number, 'extra_date': extra_date},
             url: '/Ajax/AddExtraToBooking',
             type: "POST",
             success: callback
         });
     }
 
-    function setGuests(temp_id, guests, callback) {
+    function setGuests(booking_id, guests, callback) {
         $.ajax({
-            data: { 'temp_id': temp_id, 'guests': guests },
+            data: { 'booking_id': booking_id, 'guests': guests },
             url: '/Ajax/SetGuests',
             type: "POST",
             success: callback
         });
     }
 
-    function setRooms(temp_id, guests, callback) {
+    function setRooms(booking_id, guests, callback) {
         $.ajax({
-            data: { 'temp_id': temp_id, 'guests': guests },
+            data: { 'booking_id': booking_id, 'guests': guests },
             url: '/Ajax/SetRooms',
             type: "POST",
             success: callback
         });
     }
 
-    function setDoubles(temp_id, rooms, guests, callback) {
+    function setDoubles(booking_id, rooms, guests, callback) {
         $.ajax({
-            data: { 'temp_id': temp_id, 'rooms': rooms,  'guests': guests},
+            data: { 'booking_id': booking_id, 'rooms': rooms,  'guests': guests},
             url: '/Ajax/SetDoubles',
             type: "POST",
             success: callback
